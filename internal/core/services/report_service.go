2b4849610e98d2d93780da56fd901413848ecd3e
ipincamp <support@nur-arifin.my.id>
2025-12-14 20:18:27 +0700

    feat: Implement GetAll method for fetching paginated reports

--- internal/core/services/report_service.go
+++ internal/core/services/report_service.go
@@ -28,3 +28,35 @@ func (s *ReportService) Create(ctx context.Context, reporterID string, req ports
 
 	return s.repo.Save(ctx, newReport)
 }
+
+func (s *ReportService) GetAll(ctx context.Context, req ports.FindReportsRequest) (*ports.PaginatedResponse, error) {
+	// Default Value jika tidak diisi
+	if req.Page <= 0 {
+		req.Page = 1
+	}
+	if req.Limit <= 0 {
+		req.Limit = 10 // Default 10 item per halaman
+	}
+	// Limit maksimal untuk mencegah load berlebih
+	if req.Limit > 100 {
+		req.Limit = 100
+	}
+
+	reports, total, err := s.repo.FindAll(ctx, req.Page, req.Limit)
+	if err != nil {
+		return nil, err
+	}
+
+	// Hitung Last Page
+	lastPage := int(total) / req.Limit
+	if int(total)%req.Limit != 0 {
+		lastPage++
+	}
+
+	return &ports.PaginatedResponse{
+		Data:     reports,
+		Total:    total,
+		Page:     req.Page,
+		LastPage: lastPage,
+	}, nil
+}
