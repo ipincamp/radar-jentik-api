927f16dc965e5c3e05ce42c8cc76bfe39f33cdb0
ipincamp <support@nur-arifin.my.id>
2026-06-30 08:02:37 +0700

    feat: Add StandardResponse struct and helper functions for success and error responses

---
+++ pkg/utils/response.go
@@ -0,0 +1,29 @@
+package utils
+
+import "github.com/gofiber/fiber/v2"
+
+// Format Standar JSON Response
+type StandardResponse struct {
+	Success bool        `json:"success"`
+	Message string      `json:"message"`
+	Data    interface{} `json:"data,omitempty"`
+	Errors  interface{} `json:"errors,omitempty"`
+}
+
+// Helper untuk Response Sukses
+func Success(c *fiber.Ctx, statusCode int, message string, data interface{}) error {
+	return c.Status(statusCode).JSON(StandardResponse{
+		Success: true,
+		Message: message,
+		Data:    data,
+	})
+}
+
+// Helper untuk Response Gagal / Error
+func Error(c *fiber.Ctx, statusCode int, message string, errors interface{}) error {
+	return c.Status(statusCode).JSON(StandardResponse{
+		Success: false,
+		Message: message,
+		Errors:  errors,
+	})
+}
